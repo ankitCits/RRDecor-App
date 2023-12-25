b9f1f49a82f36dbd7e406bd692c1b1f7fba355d4
Ankit Patel <ankitpatel@Ankits-MacBook-Pro.local>
2023-12-25 11:28:10 -0800

    product sub category added 2.0

--- Src/Private/Product.js
+++ Src/Private/Product.js
@@ -1,28 +1,33 @@
 import {
+  Alert,
   FlatList,
   Image,
+  Modal,
   RefreshControl,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import {ScrollView} from 'react-native-virtualized-view';
-import {useRoute} from '@react-navigation/native';
-import {useDispatch, useSelector} from 'react-redux';
-import {selectToken} from '../Redux/Slices/authSlice';
-import {selectProductData} from '../Redux/Slices/productSlice';
-import {getProductData} from '../Redux/Slices/productSlice';
+import { ScrollView } from 'react-native-virtualized-view';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectToken } from '../Redux/Slices/authSlice';
+import { selectProductData } from '../Redux/Slices/productSlice';
+import { getProductData } from '../Redux/Slices/productSlice';
 import Spinner from 'react-native-loading-spinner-overlay/lib';
+import IconD from 'react-native-vector-icons/AntDesign';
 const Product = () => {
   const route = useRoute();
   const [refreshing, setRefreshing] = useState(false);
-  const {id} = route.params;
+  const { id } = route.params;
+  const [modalOpen, setModalOpen] = useState(false)
+  const [modalData, setModalData] = useState(null)
   console.log('product is id', id);
 
   const dispatch = useDispatch();
@@ -49,50 +54,90 @@ const Product = () => {
     }, 2000);
   }, []);
 
-  const {isLoading} = useSelector(state => state.product);
+  const { isLoading } = useSelector(state => state.product);
 
-  const renderItem = ({item}) => {
+  const renderItem = ({ item }) => {
     const addUrl = item.image;
 
     return (
-      <View style={styles.itemContainer}>
-        <Image
-          source={{
-            uri: `https://rrdecor.wooshelf.com${addUrl}`,
-          }}
-          style={styles.image1}
-        />
-        <Text style={styles.text}>{item.product_name}</Text>
-      </View>
+      <TouchableOpacity
+        onPress={() => {
+          setModalData(item)
+          setModalOpen(true)
+        }}
+
+      >
+        <View style={styles.itemContainer}>
+          <Image
+            source={{
+              uri: `https://rrdecor.wooshelf.com${addUrl}`,
+            }}
+            style={styles.image1}
+          />
+
+          <Text style={styles.text}>{item.product_name}</Text>
+        </View>
+      </TouchableOpacity>
     );
   };
+
+  const MyListItem = ({ item }) => {
+    const navigation = useNavigation();
+    return (
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => {
+          setModalData(null)
+          setModalOpen(false)
+          navigation.navigate('Searching', {
+            item: item,
+          })
+        }
+        }
+
+      >
+        <View style={styles.leftContainer}>
+          <Text style={styles.title} numberOfLines={3}>
+            {item?.item_description}
+          </Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <View style={styles.iconContainer}>
+            <Image source={require('../Assets/Icons/Chevron1.png')} style={{ width: 10, height: 10 }} />
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  // console.log("productDataIs>>>>>",productDataIs[0]?.product_details)
   return (
-    <ScrollView 
-    refreshControl={
-      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-    }
-    showsVerticalScrollIndicator={false}
-    style={{backgroundColor: '#FFFF'}}>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      showsVerticalScrollIndicator={false}
+      style={{ backgroundColor: '#FFFF' }}>
       <Spinner visible={isLoading} />
       {productDataIs.length === 0 ? (
-       <View
-       style={{
-         flex:1,
-         justifyContent:'center',
-         alignItems:'center',
-         alignSelf:'center',
-         marginTop:'50%'
-       }}
-       >
-        <Image
-         source={require('../Assets/Image/norecordfound.png')}
-         style={{width: 120, height: 120}}
-         resizeMode="contain"
-       />
-       </View>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignSelf: 'center',
+            marginTop: '50%'
+          }}
+        >
+          <Image
+            source={require('../Assets/Image/norecordfound.png')}
+            style={{ width: 120, height: 120 }}
+            resizeMode="contain"
+          />
+        </View>
       ) : (
         <FlatList
-          style={{marginVertical: hp('2%')}}
+          style={{ marginVertical: hp('2%') }}
           data={productDataIs}
           renderItem={renderItem}
           keyExtractor={item => item.id}
@@ -100,6 +145,58 @@ const Product = () => {
           contentContainerStyle={styles.flatListContainer}
         />
       )}
+
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalOpen}
+        onRequestClose={() => {
+          setModalData(null)
+          setModalOpen(!modalOpen);
+        }}>
+        <View style={styles.centeredView}>
+          <TouchableOpacity
+            onPress={() => {
+              setModalData(null)
+              setModalOpen(!modalOpen)
+            }}
+            style={styles.modalViewTouch}>
+          </TouchableOpacity>
+
+          <View style={styles.modalView}>
+
+            {/* Header with close button */}
+            <View style={styles.modalHeader}>
+            <TouchableOpacity
+                onPress={() => {
+                  setModalData(null);
+                  setModalOpen(!modalOpen);
+                }}
+                style={{alignSelf:'center',}}
+                >
+                {/* <Text>Curtains</Text> */}
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  setModalData(null);
+                  setModalOpen(!modalOpen);
+                }}
+                style={{alignSelf:'center',right:wp('5%')}}
+                >
+                <IconD name="close" size={25} color="#000" />
+              </TouchableOpacity>
+            </View>
+            {/* <Text style={styles.text}>{modalData?.product_details?.length}</Text> */}
+            <FlatList
+              style={{ marginVertical: hp('2%') }}
+              data={modalData?.product_details}
+              keyExtractor={item => item?.id}
+              renderItem={({ item }) => <MyListItem item={item} />}
+            />
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
@@ -119,17 +216,19 @@ const styles = StyleSheet.create({
     height: hp('20%'),
   },
   itemContainer: {
-    width: wp('45%'),
+    width: wp('50%'),
     height: hp('30%'),
     borderRadius: 7,
     backgroundColor: '#fff',
     borderColor: '#EAEAEA',
-    // borderWidth: 1,
-    margin: 8,
-    // justifyContent: 'center',
-    // alignItems: 'center'
+    borderBottomWidth: 1,
+    borderLeftWidth: 1,
+    // margin: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
     justifyContent: 'space-evenly',
-    alignItems:'center'
+    alignItems: 'center',
+    // borderWidth:1
   },
   image1: {
     alignSelf: 'center',
@@ -145,7 +244,7 @@ const styles = StyleSheet.create({
     marginTop: 4,
     marginLeft: wp('2%'),
 
-    
+
   },
 
   flatListContainer: {
@@ -154,4 +253,102 @@ const styles = StyleSheet.create({
     paddingHorizontal: 0,
     paddingTop: 2,
   },
+  centeredView: {
+    flex: 1,
+    // justifyContent: 'flex-end',
+    alignItems: 'center',
+
+    // marginTop: 22,
+  },
+  modalViewTouch: {
+    marginTop: 0,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+
+    alignItems: 'center',
+
+    width: '100%',
+    height: wp('120')
+  },
+  modalView: {
+    marginBottom: hp('10%'),
+    marginTop: 0,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: '100%',
+    height: wp('100')
+  },
+  container: {
+    flexDirection: 'row',
+    width: '90%',
+    backgroundColor: '#fff',
+    // borderRadius: 7,
+    // borderWidth: 1,
+    // borderColor: '#EAEAEA',
+    // shadowColor: '#000',
+    // shadowOffset: {width: 0, height: 0},
+    // shadowOpacity: 0.1,
+    // shadowRadius: 5,
+    // elevation: 1,
+    height: hp('5%'),
+    alignSelf: 'center',
+    marginTop: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 0.2,
+    borderColor: 'gray'
+  },
+  leftContainer: {
+    flexDirection: 'row',
+    marginLeft: '2%',
+    width: '80%',
+    alignItems: 'center',
+
+    height: '100%',
+  },
+  title: {
+    fontSize: wp('3%'),
+
+    color: '#000',
+    marginLeft: '5%',
+
+    width: '60%',
+  },
+  rightContainer: {
+    backgroundColor: '#FFFFFF',
+    width: 20,
+    height: 20,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#F0F0F0',
+    borderWidth: 1,
+    marginRight: '2%',
+  },
+  iconContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalHeader: {
+    // flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height:50,
+    // backgroundColor:'red',
+    width:'100%',
+    borderBottomWidth:wp(0.09)
+    // padding: 10,
+  },
 });
+
+ 
